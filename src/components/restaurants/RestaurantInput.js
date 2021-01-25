e2090938bbdb3040badd91e7a38e5009750ef73d
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-01-25 13:12:47 -0800

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  
   state = {
     text: ''
   }
+
   handleOnChange = (event) => {
     this.setState({
       text: event.target.value
     });
   }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addRestaurant(this.state.text);
+    this.setState({
+      text: ''
+    });
+  }
+
   render() {
     return (
       <div>
